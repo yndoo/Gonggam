4fb76c848c3a3e711a1c86d8a9003dc9179b393a
yndoo <yndoo2k@gmail.com>
2021-12-19 23:22:12 +0900

    AI 인증 레이아웃 수정

--- appAI/src/main/java/com/example/vyw/tflite/AICamera.kt
+++ appAI/src/main/java/com/example/vyw/tflite/AICamera.kt
@@ -3,14 +3,29 @@ package com.example.vyw.tflite
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 
 class AICamera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ai_test)
 
-        // Example of a call to a native method
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.bar_aicamera2)
+        setSupportActionBar(toolbar)
+        //setHasOptionsMenu(true)
+    }
+    // 툴바의 '뒤로가기' 버튼을 눌렀을 때
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item_back -> {
+                // '뒤로가기' 버튼 눌렀을 때
+                Toast.makeText(this, "뒤로가기 click checked", Toast.LENGTH_SHORT).show();
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun btnObjectDetection_click(view: View) {
